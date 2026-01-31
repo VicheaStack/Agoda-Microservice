2225916431205082c4603b109cbeb62dde066e7d
Vichea leng <vichealeng45@gmail.com>
2026-01-31 22:41:22 +0700

    Fix: RoomMapper - add RoomRequestDTO import

--- room-service/src/main/java/com/hotel/mapper/RoomMapper.java
+++ room-service/src/main/java/com/hotel/mapper/RoomMapper.java
@@ -1,13 +1,21 @@
 package com.hotel.mapper;
 
 import com.hotel.dto.RoomDTO;
+import com.hotel.dto.RoomRequestDTO;
 import com.hotel.entity.Room;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface RoomMapper {
 
-    Room toEntity(RoomDTO dto);
+    @Mapping(source = "pricePerNight", target = "pricePerNight")
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    Room toEntity(RoomRequestDTO dto);
+
+    @Mapping(source = "pricePerNight", target = "pricePerNight")
     RoomDTO toDto(Room entity);
 
 }
