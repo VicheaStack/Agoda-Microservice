f78a91b9018e9db655dbd566863a0d62c28bfe31
Vichea leng <vichealeng45@gmail.com>
2026-03-22 00:34:50 +0700

    Fix RoomController endpoints / logic

--- room-service/src/main/java/com/hotel/controller/RoomController.java
+++ room-service/src/main/java/com/hotel/controller/RoomController.java
@@ -11,7 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/room")
+@RequestMapping("api/v1/rooms")
 public class RoomController {
 
     private final RoomService roomService;
