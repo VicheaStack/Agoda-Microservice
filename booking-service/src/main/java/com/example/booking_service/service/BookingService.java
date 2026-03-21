75321d17b13e59c384284b5e245e13f1602e49a8
Vichea leng <vichealeng45@gmail.com>
2026-03-22 00:34:45 +0700

    Update BookingService interface logic

--- booking-service/src/main/java/com/example/booking_service/service/BookingService.java
+++ booking-service/src/main/java/com/example/booking_service/service/BookingService.java
@@ -2,11 +2,13 @@ package com.example.booking_service.service;
 
 import com.example.booking_service.entity.Booking;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
 
 @Service
 public interface BookingService {
 
-    Booking create(Booking booking);
+   // Booking create(Booking booking);
+    Mono<Booking> create(Booking booking);
     Booking update(Booking booking, Long id);
     Booking getById(Long id);
     void delete(Long id);
