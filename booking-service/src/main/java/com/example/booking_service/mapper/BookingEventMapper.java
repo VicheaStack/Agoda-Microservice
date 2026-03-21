e29bf406d2ab6232370a429681c4f3d09336b6a7
Vichea leng <vichealeng45@gmail.com>
2026-03-22 00:34:42 +0700

    Fix BookingEventMapper mapping logic

--- booking-service/src/main/java/com/example/booking_service/mapper/BookingEventMapper.java
+++ booking-service/src/main/java/com/example/booking_service/mapper/BookingEventMapper.java
@@ -1,15 +1,15 @@
-package com.example.booking_service.mapper;
-
-import com.example.booking_service.dto.BookingEvent;
-import com.example.booking_service.entity.Booking;
-import org.mapstruct.Mapper;
-import org.mapstruct.NullValuePropertyMappingStrategy;
-
-@Mapper(componentModel = "spring",
-        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.SET_TO_DEFAULT)
-public interface BookingEventMapper {
-
-    BookingEvent dto(Booking entity);
-
-    Booking entity(BookingEvent dto);
-}
+//package com.example.booking_service.mapper;
+//
+//import com.example.booking_service.dto.BookingEvent;
+//import com.example.booking_service.entity.Booking;
+//import org.mapstruct.Mapper;
+//import org.mapstruct.NullValuePropertyMappingStrategy;
+//
+//@Mapper(componentModel = "spring",
+//        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.SET_TO_DEFAULT)
+//public interface BookingEventMapper {
+//
+//    BookingEvent dto(Booking entity);
+//
+//    Booking entity(BookingEvent dto);
+//}
